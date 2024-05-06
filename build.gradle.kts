a65918a76f03312d4c9e1b0d3eca827bcf422539
Alien <ztiany3@gmail.com>
2024-05-06 18:44:41 +0800

    Migrate to kotlin dsl (done).

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    alias(libs.plugins.app.common.library)
+}
+
+android {
+    namespace = "com.android.base.architecture.activity"
+}
+
+dependencies {
+    // 基础库
+    api(libs.ztiany.archdelegate)
+
+    // androidx
+    api(libs.androidx.appcompat)
+    api(libs.androidx.activity.ktx)
+    api(libs.androidx.lifecycle.runtime.ktx)
+
+    // kotlin
+    api(libs.kotlin.stdlib)
+    api(libs.kotlin.reflect)
+
+    // log
+    api(libs.jakewharton.timber)
+}
